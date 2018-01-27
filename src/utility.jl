cc35a14e1d4d78e44e4bbb82ae6834c140d4edf8
wil <wil@crofter.net>
2018-01-26 20:45:30 -0500

    debug Φ_term-typo in exponent

--- src/utility.jl
+++ src/utility.jl
@@ -1,25 +1,15 @@
-#= def phi_decay(u,n_max=100):
-    running_sum=0
-    for n in range(1,n_max+1):
-        term1=2*PI_sq*pow(n,4)*exp(9*u) - 3*PI*pow(n,2)*exp(5*u)
-        term2=exp(-1*PI*pow(n,2)*exp(4*u))
-        running_sum += term1*term2
-        #print n,term1, term2, running_sum
-    return running_sum
-=#
 
-function Φ_term{T<:AbstractFloat}(u::T,n::Int)::T
-    Tπ = T(π) # compute π to type T precision, e.g., BigFloat
-    e5u = exp(5*u)
-    return (2*(Tπ^2)*(n^4)*exp(9*u)-3*Tπ*(n^2)*e5u)*exp(-Tπ*n^2*e5u)
+function Φ_term{T<:AbstractFloat}(u::T,n::Int, PI)
+    return (2*PI^2*n^4*exp(9*u)-3*PI*n^2*exp(5*u))*exp(-PI*n^2*exp(4*u))
 end
 
-function Φ{T<:AbstractFloat}(u::T;n_max::Int=100)::T
+function Φ{T<:AbstractFloat}(u::T;n_max::Int=100, PI=T(π))
     running_sum = T(0)
     for n in 1:n_max
-        running_sum += Φ_term(u,n)
+        running_sum += Φ_term(u,n,PI)
     end
     return running_sum
 end
 
 phi_decay = Φ #alias
+KM_PI = 3.14159265358979323846
