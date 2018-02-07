f13ae5cfb5a9c8734574b89fdf1056e12d961548
wil <wil@crofter.net>
2018-02-06 19:41:57 -0500

    more prototypes relevant to Ki-Kim-Lee

--- src/KKL.jl
+++ src/KKL.jl
@@ -37,12 +37,15 @@ function ϕ4test(x;n_max=100)
 end
 
 function ϕ(x; n_max=100)
-    a=b=0.0
+    a=b=d=0.0
     for n in 1:n_max
         y = π*n^2
         d = y*exp(-y*x)
         a += d
         b += y*d
+    end
+    if d/a > 1e-6 || d/b > 1e-6
+        warn("The partial sum has not converged. n_max should be increased")
     end
     a *= -3
     b *= 2*x
@@ -56,6 +59,14 @@ function ϕ(x; n_max=100)
     return ans
 end
 
+function Ξλ_integrand(λ,u,z;n_max=100)
+    2*exp(λ*u^2+log(ΦKKL(u,n_max=n_max)))*cos(u*z)
+end
+
+function Ξλ(λ,z;n_max=100, upper_limit = 10.0)
+    return quadgk((u)-> Ξλ_integrand(λ,u,z;n_max=n_max), 0.0, upper_limit, abstol=eps(λ), maxevals=10^7)
+end
+
 function ΦKKL(u;n_max=100)
-    return 2*ϕ(exp(2*u))
+    return 2*ϕ(exp(2*u),n_max=n_max)
 end
