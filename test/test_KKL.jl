e6cce5d53089cd091edd7bba9d08b88b8529286a
wil <wil@crofter.net>
2018-02-08 12:48:53 -0500

    minor correction

--- test/test_KKL.jl
+++ test/test_KKL.jl
@@ -18,11 +18,11 @@ function test_KKL()
     @test all([isapprox(ϕ(u),ϕ(1/u),atol=1e-14) for u in x])
 
     # Verify that ϕ(big(x)) ≈ ϕ(1/big(x)) (multiprecision)
-    @test all([isapprox(ϕ(big(u)),ϕ(1/big(u)),atol=1e-14) for u in x])
+    @test all([isapprox(ϕ(big(u)),ϕ(1/big(u)),atol=1e-16) for u in x])
 
     # Verify that two implementations of ϕ are approximately equal unless
     # their difference is less than 1e-14, which seems the best we can
-    # do with Float64. Multiprecision does slightly better but not much.
+    # do with Float64. As above, multiprecision does slightly better but not much.
     @test all([isapprox(ϕ(u),DBNUpperBound.ϕ4test(u),atol=1e-14) for u in x])
 
     
