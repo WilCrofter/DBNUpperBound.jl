0cad041374fd4c5a27233970eca9afcecee24ad7
wil <wil@crofter.net>
2018-02-09 14:05:43 -0500

    test that Ht(0.0,t)≈1/2*Ξ(0.0,t/2) where Ht(0.0,t) is as defined in PM15, Ξ(0.0,t) as in KKL2009

--- test/test_KKL.jl
+++ test/test_KKL.jl
@@ -27,10 +27,19 @@ function test_KKL()
 
     # KKL2009 claims that Ξ(0.0,t) = Ξ(t) = ξ(1/2 + t*im), but as those functions
     # are implemented here, following KKL2009 for implementation of Ξ(0.0,t)
-    # and the using a standard definition of Riemann's xi, for implementation
-    # of ξ, the actual relation seems to be Ξ(0.0,t) = ξ(1/2 + t*im)/4 = Ξ(t)
+    # and using a standard definition of Riemann's xi for implementation
+    # of ξ, the actual relation seems to be Ξ(0.0,t) = ξ(1/2 + t*im)/4 = Ξ(t)/4.
     # where  Ξ(t) is the Riemann-Landau Xi.
     @test all([Ξ(0.0,t)[1] ≈ ξ(1/2+im*t)/4 for t in x])
+    # Equivalently
+    @test all([Ξ(0.0,t)[1] ≈ Ξ(t)/4 for t in x])
+
+    # Assuming Ξ(0.0,t) = ξ(1/2 + t*im)/4 where Ξ(0.0,t) is as defined in KKL2009
+    # and noting that, as Ht(0.0,t) is defined at PM15, Ht(0.0,t)= H0(t) =ξ((1+t*im)/2)/8,
+    # and that ξ((1+t*im)/2)/8 = 1/2*ξ(1/2+t/2*im))/4 = 1/2*Ξ(0.0,t/2), we should
+    # have Ht(0.0,t)=H0(t)=1/2*Ξ(0.0,t/2). We know from test_Ht.jl that
+    # the first equality holds within numerical tolerance. Checking the second equality:
+    @test all([H0(t)≈1/2*Ξ(0.0,t/2)[1] for t in x])
 end
 
 test_KKL()
