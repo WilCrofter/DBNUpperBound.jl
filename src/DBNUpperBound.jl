54fd6b04c4891bb88b168ffb32d827f7c4cc5a25
wil <wil@crofter.net>
2018-05-12 11:10:13 -0400

    remove deprecated code

--- src/DBNUpperBound.jl
+++ src/DBNUpperBound.jl
@@ -3,25 +3,8 @@ module DBNUpperBound
 using QuadGK
 using SpecialFunctions
 
-Γ = SpecialFunctions.gamma
+# Γ = SpecialFunctions.gamma
 
-# utility.jl and Ht.jl
-# export phi_decay, Φpm, Ht, ζ, zeta, Γ, gamma, ξ, xi, XiRL, ΞRL,H0
-# KKL.jl
-# export ψKKL, ϕKKL, ΦKKL, ΞKKL
-# Ht_asymptotics.jl
-# export A, B, C
-
-# include("types.jl")
-# include("special_fcts.jl") 
-# include("utility.jl")
-# include("Ht.jl")
-
-# Submodules
-# include("KKL/KKL.jl")
-# using .KKL
-# include("asymptotics/asymptotics.jl")
-# using .Asymptotics
 include("pm15a/PM15a.jl")
 using .PM15a
 
