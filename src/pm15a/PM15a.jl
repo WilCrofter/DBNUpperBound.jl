7485c1c38d33ce67a3ac53835fbdc1d3a5aaa813
wil <wil@crofter.net>
2018-05-27 11:07:30 -0400

    add draft pdf on which PM15a is based (using git lfs)

--- src/pm15a/PM15a.jl
+++ src/pm15a/PM15a.jl
@@ -1,3 +1,9 @@
+""" PM15a
+    
+    A submodule based on a draft version of "EFFECTIVE APPROXIMATION OF HEAT FLOW EVOLUTION OF THE RIEMANN XI FUNCTION, AND AN UPPER BOUND FOR THE DEBRUIJN-NEWMAN CONSTANT" by D.H.J. POLYMATH.
+
+    The draft version, referred to as "the reference" in PM15a docs, is that of commit 0a0f729 of the github.com/torth/dbn_upper_bound. The original may be found at url https://github.com/teorth/dbn_upper_bound/blob/0a0f72933da63a9589c42e3d404f4fc5b88060a7/Writeup/debruijn.pdf. There is also a copy in this module.
+    """
 module PM15a
 
 using DBNUpperBound
