2fd1f559792436feea9de47ba785d99b0ddbb9c8
wil <wil@crofter.net>
2018-05-31 14:43:29 -0400

    add top level module which was somehow untracked.

---
+++ src/DBNUpperBound.jl
@@ -0,0 +1,44 @@
+module DBNUpperBound
+
+using QuadGK
+using SpecialFunctions
+using Primes
+
+include("pm15a/PM15a.jl")
+using .PM15a
+
+# pm15a/utilities.jl
+export ζ, ξ
+
+# pm15a/applying_the_fundamental_solution_for_the_heat_equation.jl
+export Hₜ
+
+# pm15a/dynamics_of_zeros.jl
+export r₀, rₜₙ_integrand, rₜₙ_by_integration
+
+# pm15a/introduction.jl
+export s⁺, M₀, logM₀, logM₀′, α, α′, Mₜ, B₀, bᵗₙ, logbᵗₙ
+export region_5, in_region_5
+export γₜ, κ, fₜ
+export N, H̃, H̃₂
+
+# pm15a/notation
+export complex_power, gaussian_identity # temporarily, anyway
+
+# pm15a/estimates_for_large_x
+export ϵₜₙ, ϵ̃ₜₙ, ϵ̃, rₜₙ, RtN, A, B, C, EA, EB, EC, EC₀
+export eA, eB, eC, eC₀
+export Aterm, Bterm
+
+# pm15a/bounding_dirichlet_series
+export Dirichlet_convolution, mollifiers, αterm
+export bound77, bound78
+
+# pm15a/a_new_upper_bound.jl
+export δ₁, δ₁_ub, bound23, bound23a, util_23b, bound23b, bound85
+
+
+
+
+    
+end # module
