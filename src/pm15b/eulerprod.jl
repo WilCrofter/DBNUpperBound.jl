7f97b77cfb5ce47b65d5a20d9a8255578c7ae2e9
wil <wil@crofter.net>
2018-07-23 19:18:24 -0400

    complete and time ad hoc procedure

--- src/pm15b/eulerprod.jl
+++ src/pm15b/eulerprod.jl
@@ -13,7 +13,7 @@ end
 
 """ adhoc(;X::T=6e10, n::I=27, Q::I=10^5, set::Vector{T}=[-0.5,0.0,0.5], threshold::Real=4.0) where {T<:Real, I<:Integer}
 
-    Execute the ad hoc procedure described on pp. 32-33 of the reference to determine placement of the barrier.
+    Execute the ad hoc procedure described on pp. 32-33 of the reference to determine placement of the barrier, returning accepted values of q and corresponding values of |f₀| as a 2-tuple. 
 
     X (default 6x10¹⁰) approximate location of the barrier.
     n (default 27) upper limit of primes to include in the euler product
@@ -21,8 +21,26 @@ end
     set (default [-0.5,0.0,0.5]) array of allowable values for x-X-q q∈{1,…,Q}
     threshold (default 3.944) acceptance threshold for values of eulerproduct(x-X-q)
 
+    TIMING (2 core, 3.6 GHz, evaluation of f₀ accounting for most.) 
+    julia> @time q,f=M.adhoc()
+    275.663969 seconds (846.60 M allocations: 27.724 GiB, 10.44% gc time)
+
     NOTE: The reference reports that a threshold of 4.0 returns the 7 integers 1046, 22402, 24198, 52806,
-77752, 83952, and 99108. With a threshold of 4.0 this function returns only the first 5 of these. With the default threshold, this function returns 11 which include the reported 7.
+77752, 83952, and 99108. With a threshold of 4.0 the current function returns only the first 5 of these. With the default threshold, 3.944, this function returns 11 which include the reported 7. The heuristically optimal value of q, that which maximizes the minimum value of |f₀(X+q+r+i)| where r ∈ set and i=√(-1), is the same however.
+
+     q       f₀
+  1046  4.24874
+ 22402  4.2109 
+ 24198  4.28588
+ 52806  4.11698
+ 57701  3.68531
+ 61762  4.18553
+ 72114  3.9124 
+ 72857  3.444  
+ 77752  3.93115
+ 83952  4.31925 *
+ 99108  3.80751
+
     """
 function adhoc(;X::T=6e10, n::I=27, Q::I=10^5, set::Vector{T}=[-0.5,0.0,0.5], threshold::Real=3.944) where {T<:Real, I<:Integer}
     accepted = spzeros(Bool,Q)
@@ -36,6 +54,14 @@ function adhoc(;X::T=6e10, n::I=27, Q::I=10^5, set::Vector{T}=[-0.5,0.0,0.5], th
             accepted[q]=true
         end
     end
-    return find(accepted)
+    accepted=find(accepted)
+    f₀=Vector{typeof(X)}(length(accepted))
+    for i in eachindex(accepted)
+        for j in eachindex(set)
+            tmp[j] = abs(fₜ(0,X+accepted[i]+set[j]+im)[1])
+        end
+        f₀[i]=minimum(tmp)
+    end
+    return accepted, f₀
 end
     
